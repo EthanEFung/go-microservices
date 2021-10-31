246b22d7ece4be21a2608a1ff49c6703a0941735
Ethan Fung <ethanfung@ethans-mbp-2.lan>
2021-10-31 07:48:30 -0700

    ep4

--- handlers/products.go
+++ handlers/products.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
 	"rest/data"
+	"strconv"
 )
 
 type Products struct {
@@ -19,6 +21,30 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.getProducts(rw, r)
 		return
 	}
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+	if r.Method == http.MethodPut {
+		// expect the id in the URI
+		reg := regexp.MustCompile(`/([0-9)+)/`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "invalid URI", http.StatusBadRequest)
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invlid URI", http.StatusBadRequest)
+		}
+		p.updateProducts(id, rw, r)
+	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
@@ -26,5 +52,35 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	if err := lp.ToJSON(rw); err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST product")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusInternalServerError)
+	}
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusInternalServerError)
+	}
+	err = data.UpdateProduct(id, prod)
+
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Produt not found", http.StatusInternalServerError)
 	}
 }
