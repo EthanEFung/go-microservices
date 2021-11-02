c0708830068cc879f9ea62f98df51e1e684a6cb1
Ethan Fung <ethanfung@ethans-mbp-2.lan>
2021-11-02 08:35:30 -0700

    ep6 JSON validation

---
+++ data/products_test.go
@@ -0,0 +1,15 @@
+package data
+
+import "testing"
+
+func TestValidation(t *testing.T) {
+	p := &Product{
+		Name: "Frappe",
+		Price: 1,
+		SKU: "abs-b-cs",
+	}
+	err := p.Validate()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module rest
 
 go 1.13
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/go-playground/validator/v10 v10.9.0
+	github.com/gorilla/mux v1.8.0
+)
